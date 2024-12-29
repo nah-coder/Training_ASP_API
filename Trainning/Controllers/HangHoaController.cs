f9cd8832fa208c717f149bfcb62695d91199fa0b
nah-coder <huyanhnguyen1211@gmail.com>
2024-12-29 22:28:18 +0700

    crud

---
+++ Trainning/Controllers/HangHoaController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Trainning.Models;
+
+namespace Trainning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+
+        public static List<hangHoa> hanghoas = new List<hangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hanghoas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                var hanghoa = hanghoas.FirstOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hanghoa == null)
+                    return NotFound();
+                return Ok(hanghoa);
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(HangHoaVM vm)
+        {
+            var hanghoa = new hangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = vm.TenHangHoa,
+                DonGia = vm.DonGia,
+            };
+            hanghoas.Add(hanghoa);
+            return Ok(new
+            {
+                Success = true,Data = hanghoa
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id,hangHoa hangHoaEdit)
+        {
+            try
+            {
+                var hanghoa = hanghoas.FirstOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hanghoa == null)
+                    return NotFound();
+                if(id!= hanghoa.MaHangHoa.ToString())
+                    return BadRequest();
+                hanghoa.TenHangHoa = hangHoaEdit.TenHangHoa;
+                hanghoa.DonGia = hangHoaEdit.DonGia;
+                return Ok(hanghoa);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            try
+            {
+                var hanghoa = hanghoas.FirstOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hanghoa == null)
+                    return NotFound();
+                hanghoas.Remove(hanghoa);
+                return Ok(hanghoa);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Trainning/Models/HangHoaVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Trainning.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+    public class hangHoa : HangHoaVM { 
+        public Guid MaHangHoa { get; set;}
+    }
+}
